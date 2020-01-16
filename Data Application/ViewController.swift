94799aa0511317f730f9e0317e4153f43abd4902
richa <Riyu2498.patel@gmail.com>
2020-01-16 17:12:42 -0500

    Initial Commit

---
+++ Data Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data Application
+//
+//  Created by MacStudent on 2020-01-16.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
